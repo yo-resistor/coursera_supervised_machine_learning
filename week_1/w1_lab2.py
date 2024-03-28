0082266d67718d4016ecd10c9831132e237f16eb
Yunsik Ohm <yunsikohm@gmail.com>
2024-03-28 10:16:50 -0700

    changed names of files

--- week_1/w1_lab2.py
+++ week_1/w1_lab2.py
@@ -1,80 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# plt.style.use('./deeplearning.mplstyle')
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
 
-# x_train is the input variable (size in 1000 square feet)
-# y_train is the target (price in 1000s of dollars)
-x_train = np.array([1.0, 2.0])
-y_train = np.array([300.0, 500.0])
-print(f"x_train = {x_train}")
-print(f"y_train = {y_train}")
+x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
+y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
 
-# m is the number of training examples
-print(f"x_train.shape: {x_train.shape}")
-m = x_train.shape[0]
-print(f"Number of training examples is: {m}")
-
-# m is the number of training examples
-m = len(x_train)
-print(f"Number of training examples is: {m}")
-
-i = 0 # Change this to 1 to see (x^1, y^1)
-
-x_i = x_train[i]
-y_i = y_train[i]
-print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
-
-# Plot the data points
-plt.scatter(x_train, y_train, marker='x', c='r')
-# Set the title
-plt.title("Housing Prices")
-# Set the y-axis label
-plt.ylabel('Price (in 1000s of dollars)')
-# Set the x-axis label
-plt.xlabel('Size (1000 sqft)')
-plt.show()
-
-w = 100
-b = 100
-print(f"w: {w}")
-print(f"b: {b}")
-
-def compute_model_output(x, w, b):
-    '''
-    Computes the prediction of a linear model
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
     Args:
         x (ndarray (m,)): Data, m examples 
+        y (ndarray (m,)): target values
         w,b (scalar)    : model parameters  
-    Returns:
-        f_wb (ndarray (m,)): model prediction
-    '''
-    m = x.shape[0]
-    f_wb = np.zeros(m)
-    for i in range(m):
-        f_wb[i] = w * x[i] + b
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+        to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
         
-    return f_wb
+    total_cost = (1 / (2 * m)) * cost_sum  
 
-tmp_f_wb = compute_model_output(x_train, w, b,)
+    return total_cost
 
-# Plot our model prediction
-plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+plt_intuition(x_train,y_train)
 
-# Plot the data points
-plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+# larger data set
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480,  430,   630, 730,])
 
-# Set the title
-plt.title("Housing Prices")
-# Set the y-axis label
-plt.ylabel('Price (in 1000s of dollars)')
-# Set the x-axis label
-plt.xlabel('Size (1000 sqft)')
-plt.legend()
-plt.show()
+plt.close('all') 
+fig, ax, dyn_items = plt_stationary(x_train, y_train)
+updater = plt_update_onclick(fig, ax, x_train, y_train, dyn_items)
 
-w = 200                         
-b = 100    
-x_i = 1.2
-cost_1200sqft = w * x_i + b    
-
-print(f"${cost_1200sqft:.0f} thousand dollars")
+soup_bowl()
